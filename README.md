8bbf99054c2ec955560dff9241cd5d18f38883a0
zacheryasc <zacheryasc@gmail.com>
2023-06-30 08:41:44 +0000

    basics

---
+++ README.md
@@ -0,0 +1,6 @@
+Just grab my script
+
+```
+wget https://raw.githubusercontent.com/zacheryasc/misc-tools/master/bootstrap.sh
+```
+
--- bootstrap.sh
+++ bootstrap.sh
@@ -4,6 +4,27 @@ base() {
 	sudo apt update
 	sudo apt install --assume-yes build-essential
 	sudo apt install --assume-yes git clang curl libssl-dev llvm libudev-dev make protobuf-compiler gcc musl-dev tree
+	
+	# git config
+	git config --global user.name "zacheryasc"
+	git config --global user.email "zacheryasc@gmail.com"
+
+}
+
+rust-setup() {
+  # rust base
+	curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs > $(pwd)/rustup.sh
+	sh $(pwd)/rustup.sh -y
+	rm $(pwd)/rustup.sh
+	source $HOME/.cargo/env
+}
+
+fedimint() {
+	rust-setup
+	curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | sh -s -- install
+	git clone git@github.com:fedimint/fedimint.git
+	cd fedimint
+	./scripts/bootstrap.sh
 }
 
 if (return 0 2>/dev/null); then
